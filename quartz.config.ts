a4f9e24b3238e3e1ad9b08e7867380a67dedc48e
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2025-11-16 17:09:30 +0000

    Quartz sync: Nov 16, 2025, 5:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -64,7 +64,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true, enableCheckbox: true, wikilinks: true }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true, enableCheckbox: true, wikilinks: true, enableImageWidth: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
